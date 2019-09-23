b514f024e91be2e0d35a4af38ae4519a0c152e2b
Alex Raymond <alexanderraymond439@gmail.com>
2019-09-23 18:31:30 -0400

    Almost done, cant figure out how to place tiger image js into its own directory as the images cant be found once i do so

--- Javascripts/Rawr.js
+++
@@ -1,2 +0,0 @@
-<img src="webserversRIPO1/Images/TigerClose.png" onmouseover="this.src='webserversRIPO1/Images/TigerOpen.png'" onmouseout="this.src='webserversRIPO1/Images/TigerClose.png'" />
-        
